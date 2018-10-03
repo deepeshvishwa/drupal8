44d2099e24ba58d882d6a8b2a9c76ebec2bb423f
deepeshvishwa <deepesh.vishwkarma@wdadc.com>
2018-10-03 22:01:06 +0530

    add user step 1 module

--- modules/custom/user_step1/src/Form/UserStep1Form.php
+++ modules/custom/user_step1/src/Form/UserStep1Form.php
@@ -3,7 +3,7 @@ namespace Drupal\user_step1\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
-
+use Drupal\user\Entity\User;
 class UserStep1Form extends FormBase {
 
   public function getFormId() {
@@ -14,6 +14,23 @@ class UserStep1Form extends FormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
+
+//$user = \Drupal::currentUser();
+$user_load = User::load(\Drupal::currentUser()->id());
+
+$email = $user_load->get('mail')->value;
+$name = $user_load->get('name')->value;
+$full_name = $user_load->get('field_fullname')->value;
+$mobile = $user_load->get('field_mobile_number')->value;
+
+print 'Email-'.$email.',<br/> Name-'.$name.',<br/> Full Name-'.$full_name.',<br/> Mobile No-'.$mobile;
+
+exit;
+
+    $form['user_full_name'] = array(
+      '#type' => 'markup',
+      '#markup' => 'Vishwkarma',
+    );
 
     $form['professional_category'] = array (
       '#type' => 'select',
